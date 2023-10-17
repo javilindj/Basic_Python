7ce877cbeabaeacb9421569ee8596c25a92dfa46
Javier Gallego <javigallegofenor@gmail.com>
2023-10-17 17:16:50 +0200

    Update 7- Listas.py
    Actualizo el fichero listas.py

--- 7- Listas.py
+++ 7- Listas.py
@@ -1,6 +1,6 @@
 miLista=["Javi","Antonio","Pedro",3,5] * 3 # Poniendo el multiplicador al final, se multiplica la lista por el número que le indicamos
 
-#miLista.append("Cesar") Añadimos al final de la lista 
+miLista.append("Cesar") # Añadimos al final de la lista 
 
 #miLista.insert(1,"Juan") Para insertar a un elemento a la lista, nos pide índice y elemento.
 
